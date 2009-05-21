0e8407f2919e6378033e9800d91ae7e5c9c34efc
fmavituna <fmavituna@3d7cce66-4640-11de-84a6-839b6ed6c72e>
2009-05-21 21:13:03 +0000

    Initial commit google code baby...

---
+++ trunk/DBF/Helpers.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Net.Security
+Imports System.Net
+
+Public Module Helpers
+
+    ''' <summary>
+    ''' Validates the certificates (accept any of them).
+    ''' </summary>
+    ''' <param name="sender">The sender.</param>
+    ''' <param name="certificate">The certificate.</param>
+    ''' <param name="chain">The chain.</param>
+    ''' <param name="sslPolicyErrors">The SSL policy errors.</param>
+    ''' <returns><c>Always True</c></returns>
+    Public Function ValidateCertificate(ByVal sender As Object, ByVal certificate As System.Security.Cryptography.X509Certificates.X509Certificate, ByVal chain As System.Security.Cryptography.X509Certificates.X509Chain, ByVal sslPolicyErrors As SslPolicyErrors) As Boolean
+        'Accept any certificate
+        Return True
+    End Function
+
+    ''' <summary>
+    ''' Parses the specified name.
+    ''' </summary>
+    ''' <param name="RawString">The raw string.</param>
+    ''' <param name="Name">The name.</param>
+    ''' <param name="Value">The value.</param>
+    Public Sub ParseParameter(ByVal RawString As String, ByRef Name As String, ByRef Value As String, Optional ByVal Identifier As Char = "="c)
+        Dim ValPos As Integer = RawString.IndexOf(Identifier)
+
+        'Not Found
+        If ValPos = -1 Then Exit Sub
+
+        Name = RawString.Substring(0, ValPos)
+        Value = RawString.Substring(ValPos + 1, RawString.Length - ValPos - 1)
+    End Sub
+
+End Module
---
+++ trunk/DBF/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Ferruh Mavituna's Freaking Simple Fuzzer")> 
+<Assembly: AssemblyDescription("A Freaking Simple Fuzzer")> 
+<Assembly: AssemblyCompany("Ferruh Mavituna")> 
+<Assembly: AssemblyProduct("FSF")> 
+<Assembly: AssemblyCopyright("Copyright © 2007-2009 - Ferruh Mavituna")> 
+<Assembly: AssemblyTrademark("Ferruh Mavituna")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("73e7c177-e31c-4e28-bf73-c22c69972a1b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("7.1.0.0")> 
+<Assembly: AssemblyFileVersion("7.1.0.0")> 
